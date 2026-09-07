ede16bffda6f6effabc678038972451b0b37e717
playfairs <74739795+playfairs@users.noreply.github.com>
2026-09-07 12:13:44 -0500

    chore: remove obsolete CLI test

--- tests/cli.rs
+++
@@ -1,9 +0,0 @@
-use std::process::Command;
-
-#[test]
-fn binary_reports_help() {
-  let binary = env!("CARGO_BIN_EXE_nox");
-  let output = Command::new(binary).arg("help").output().unwrap();
-  assert!(output.status.success());
-  assert!(String::from_utf8_lossy(&output.stdout).contains("The Nox Build System"));
-}
