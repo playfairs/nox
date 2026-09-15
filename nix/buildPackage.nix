8a509deaeb062f4f272586004abb097aa106b9b2
playfairs <root@playfairs.cc>
2026-09-15 18:20:32 -0500

    ok

--- nix/buildPackage.nix
+++ nix/buildPackage.nix
@@ -5,6 +5,11 @@ pkgs.rustPlatform.buildRustPackage {
   pname = "nox";
   version = pkgs.lib.strings.trim (builtins.readFile ../VERSION);
   src = ../.;
-  cargoLock.lockFile = ../Cargo.lock;
+  cargoLock = {
+    lockFile = ../Cargo.lock;
+    outputHashes = {
+      "noml-0.1.0" = "sha256-dykjiEdvzIqlHhO5XyamJAG7ztvsZlsrBekj11iCo8c=";
+    };
+  };
   meta.mainProgram = "nox";
 }
